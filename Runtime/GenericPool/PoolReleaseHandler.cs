0ce3eedadf5fdf90d6eb7937628bbc12fe0088fe
Aleksey Rastegaev <hitomilras@gmail.com>
2020-02-12 12:58:25 +0300

    Added PoolRreleaseHandler

---
+++ Runtime/GenericPool/PoolReleaseHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class PoolReleaseHandler<T> : MonoBehaviour
+{
+
+    private UnityEvent OnRelease = new UnityEvent();
+
+    public void Init(UnityEngine.Events.UnityAction onReleaseAction)
+    {
+        OnRelease.AddListener(onReleaseAction);
+    }
+}
