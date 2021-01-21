145237ef29138337b1f1c27f4cb6251570c1102d
rastleks <hitomilras@yandex.ru>
2021-01-21 20:52:55 +0300

    Added simple orbit camera

---
+++ Runtime/Cameras/OrbitCamera.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+
+    public float SensitivityX = 15f;
+
+    public float SensitivityY = 15f;
+
+    public float OrbitDistance = 1f;
+
+    public Vector2 InitialRotation;
+
+    public Transform OrbitCenter;
+
+    private float rotationX;
+    private float rotationY;
+
+    private Vector3 lastMousePosition;
+
+    private void Awake()
+    {
+        rotationX = InitialRotation.x;
+        rotationY = InitialRotation.y;
+    }
+
+    void Update()
+    {
+        Vector2 delta = Vector2.zero;
+
+        if (lastMousePosition == Vector3.zero)
+            lastMousePosition = Input.mousePosition;
+
+        if (Input.touchCount == 1)
+            delta = Input.GetTouch(0).deltaPosition;
+        else if (Input.GetMouseButton(0))
+        {
+            delta.x = Input.GetAxis("Mouse X");
+            delta.y = Input.GetAxis("Mouse Y");
+        }
+
+        rotationX += delta.x * SensitivityX * Time.deltaTime;
+        rotationY += delta.y * SensitivityY * Time.deltaTime;
+
+        var lookRotation = Quaternion.Euler(rotationY, rotationX, 0);
+
+        transform.position = OrbitCenter.position + lookRotation * (Vector3.back * OrbitDistance);
+        transform.rotation = lookRotation;
+
+        lastMousePosition = Input.mousePosition;
+    }
+}
